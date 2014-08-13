9f0fdb6dd127580b28c643168e37e13caef71d41
sweeseng789 <sweeseng123@gmail.com>
2014-08-13 12:31:43 +0800

    Have Fun

---
+++ have fun/have fun/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+
+void main()
+{
+	cout << "                    MM                          MMMMM \n";
+    cout << "              =M         M                ,,MN      $MM\n";
+    cout << "             M       M     M+         MMMMMMMM=~MM  M   MM\n";
+    cout << "            M     M   M    M                  :MM8        M\n ";
+    cout << "           M       M   I                            M      M\n";
+    cout << "           M      M  M             M                 M     ?\n";
+    cout << "          M             MMMMMMMMM          M                M\n";
+    cout << "       MMMMMM     N ,M MMMMM     M          MMMM, MM        M=\n";
+    cout << "     MM           ?  MD      MM   M     MMMMMMMM           M  M\n";
+    cout << "     MMM   MM   MM        MI               M                    M\n";
+    cout << "    MZM   M        MMMMMM=                 M          MM ZM   + M\n";
+    cout << "    MM   M    MM                            M      MMM       M  M\n";
+    cout << "    MD   M  ,MM  MM            MMM8           M         ?   M M M\n";
+    cout << "    MMM  M    M     8MM         M  MMM        MM M     MM    M M\n";
+    cout << "     MMM       M?    M   MMM                MM        MMMM    MM\n";
+    cout << "      MM M,     MMMZ MM       MMM         MD        MM MMM    M\n";
+    cout << "       MM        M   MMM      M     MMMMMM$$$IMMMMM   ? MM   M\n";
+    cout << "         M        M   M MMMM$ M       M     M     M   M MM   M\n";
+    cout << "          M        M M     MMMMMMM    M     M    IMMMMMMMM   M\n";
+    cout << "          MI         M       M  8MMMMMMMMMMMMMMMMMMMMMMMMM   M\n";
+    cout << "          M          MM     M       MMMMMMMMMMMMMMMMMMMMM   M\n";
+    cout << "             M           M  M        M     M    M  M? M M    M\n";
+    cout << "              MM  M  M     MMM       M     M   8=  M  MM     M\n";
+    cout << "                MM   M  M       MMMMMM    MM  MMMMMMM        M\n";
+    cout << "                 8M   M    M                          M     ~\n";
+    cout << "                     MM    :M   .M     ,MMM.                  M\n";
+    cout << "                         MM       M,                    M     M\n";
+    cout << "                            MM            ~MMMMMMMMM         D,\n";
+    cout << "                               MMI                           M\n";
+    cout << "                                   MMMM,                   M=\n";
+    cout << "                                          MMMM         ?MM\n     ";       
+                                                                    
+}
